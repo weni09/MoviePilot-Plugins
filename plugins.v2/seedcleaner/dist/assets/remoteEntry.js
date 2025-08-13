2444b59d05d0b328f2b3dfb5b9913ad04b1adfde
weni09 <weni09@163.com>
2025-08-13 23:25:19 +0800

    。

--- plugins.v2/seedcleaner/dist/assets/remoteEntry.js
+++ plugins.v2/seedcleaner/dist/assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Page":()=>{
-      dynamicLoadingCss(["__federation_expose_Page-CkN_icgQ.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-CY_ms-AS.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["__federation_expose_Page-CpgKJ9mg.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-C00Y21Ju.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Config":()=>{
       dynamicLoadingCss(["__federation_expose_Config-BUbODzfo.css"], false, './Config');
       return __federation_import('./__federation_expose_Config-BiIK4Gk1.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
